143b3870f3f598d13635edc7f955d7ff402f060a
caesar <2569751179@qq.com>
2020-01-18 16:21:16 +0800

    first commit

---
+++ registry/eureka/src/main/java/org/caesar/cloud/registry/eureka/RegistryEurekaApplication.java
@@ -0,0 +1,14 @@
+package org.caesar.cloud.registry.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+@SpringBootApplication
+@EnableEurekaServer
+public class RegistryEurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RegistryEurekaApplication.class,args);
+    }
+}
